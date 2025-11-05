869ed70f2629e665a32bef00868b191b302beb27
Ruhan Malik <aruhanmalik@gmail.com>
2025-11-04 23:51:49 -0600

    got react installed and working

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,7 +10,7 @@ if (started) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 1000,
+    width: 800,
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div>
+            <h1>Hello, world from react at the home page</h1>
+        </div>
+    );
+}
