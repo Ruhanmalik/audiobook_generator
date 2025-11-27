d7d83b9d17540a609dd6cc9186bb81314f0125a3
Ruhan Malik <aruhanmalik@gmail.com>
2025-11-27 04:52:34 -0600

    able to use alter the text in memory

--- frontend/src/pages/Audio.jsx
+++
@@ -1,7 +0,0 @@
-export default function Audio() {
-  return (
-    <div>
-      <h1>Audio</h1>
-    </div>
-  );
-}
--- frontend/src/pages/Text.jsx
+++
@@ -1,7 +0,0 @@
-export default function Text() {
-  return (
-    <div>
-      <h1>Text</h1>
-    </div>
-  );
-}
